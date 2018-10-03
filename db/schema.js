0f622701cfd15dd3b2ee2255fb22b8ec22cf541e
miknhoj <kimjohan86@gmail.com>
2018-10-03 12:31:08 -0400

    addded schema, model and seeds

---
+++ db/schema.js
@@ -0,0 +1,17 @@
+const Schema = require('mongoose').Schema
+
+const IdeaSchema = new Schema({
+    title: String,
+    descriptino: String
+})
+
+const UserSchema = new Schema({
+    userName: String,
+    password: String,
+    ideas: [IdeaSchema]
+})
+
+module.exports = {
+    UserSchema,
+    IdeaSchema
+}
