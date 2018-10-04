412ff72eb6aa364b774316be06e3e35fe1e8828d
miknhoj <kimjohan86@gmail.com>
2018-10-04 18:48:41 -0400

    added some css - made input fields transparent

--- client/src/components/IdeaBoard.js
+++ client/src/components/IdeaBoard.js
@@ -29,10 +29,11 @@ const StyledIdeaList = styled.div`
   font-size: 3vw;
   flex-wrap: wrap;
 `
-const StyledNav = styled.div `
+const StyledNav = styled.div`
   display: flex;
   font-size: 15px;
   height: 40px;
+  margin: 10px;
 `
 const StyledSort = styled.div`
   font-size: 15px;
@@ -40,78 +41,90 @@ const StyledSort = styled.div`
   font-weight: bold;
 `
 
+const StyledTitle = styled.input`
+  font-weight: bold;
+  background-color: transparent;
+  border: 0px;
+`
+const StyledDescription = styled.input`
+  background-color: transparent;
+  border: 0px;
+`
+const StyledDelete = styled.div`
+  font-size: .5em;
+`
 export default class IdeaBoard extends Component {
-    state = {
-        user: {},
-        ideas: []
-    }
+  state = {
+    user: {},
+    ideas: []
+  }
 
-    getUser = async() => {
-      const userId = this.props.match.params.userId
-      const response = await axios.get(`/api/users/${userId}`)
-      this.setState({ 
-        user: response.data,
-        ideas: response.data.ideas.reverse()
-      })
-    }
+  getUser = async () => {
+    const userId = this.props.match.params.userId
+    const response = await axios.get(`/api/users/${userId}`)
+    this.setState({
+      user: response.data,
+      ideas: response.data.ideas.reverse()
+    })
+  }
 
-    componentDidMount = () => [
-      this.getUser()
-    ]
+  componentDidMount = () => [
+    this.getUser()
+  ]
 
-    handleNew = async () => {
-      const userId = this.props.match.params.userId
-      const newIdea = await axios.post(`/api/users/${userId}/ideas`)
-      console.log(newIdea)
-      await this.getUser()
-    }
+  handleNew = async () => {
+    const userId = this.props.match.params.userId
+    const newIdea = await axios.post(`/api/users/${userId}/ideas`)
+    console.log(newIdea)
+    await this.getUser()
+  }
 
-    handleDelete = async (ideaId) => {
-      const userId = this.props.match.params.userId
-      await axios.delete(`/api/users/${userId}/ideas/${ideaId}`)
-      await this.getUser()
-    }
+  handleDelete = async (ideaId) => {
+    const userId = this.props.match.params.userId
+    await axios.delete(`/api/users/${userId}/ideas/${ideaId}`)
+    await this.getUser()
+  }
 
-    handleChange = (event, i) => {
-      const ideas = [...this.state.ideas]
-      ideas[i][event.target.name] = event.target.value
-      this.setState({ ideas })
-    }
+  handleChange = (event, i) => {
+    const ideas = [...this.state.ideas]
+    ideas[i][event.target.name] = event.target.value
+    this.setState({ ideas })
+  }
 
-    updateIdea = async (i) => {
-      const userId = this.props.match.params.userId
-      const updatedIdea = this.state.ideas[i]
-      await axios.put(`/api/users/${userId}/ideas/${updatedIdea._id}`, updatedIdea)
-      
-    }
+  updateIdea = async (i) => {
+    const userId = this.props.match.params.userId
+    const updatedIdea = this.state.ideas[i]
+    await axios.put(`/api/users/${userId}/ideas/${updatedIdea._id}`, updatedIdea)
+
+  }
 
   render() {
     const ideasList = this.state.ideas.map((idea, i) => {
       return (<StyledIdea key={i}>
-      <div onClick={() => this.handleDelete(idea._id)}>
-        X
-      </div>
+        <StyledDelete onClick={() => this.handleDelete(idea._id)}>
+          X
+      </StyledDelete>
 
-      <input type='text' name='title' value={idea.title} 
-        onChange={(event) => this.handleChange(event, i)} 
-        onBlur={() => this.updateIdea(i)}/>
+        <StyledTitle type='text' name='title' value={idea.title}
+          onChange={(event) => this.handleChange(event, i)}
+          onBlur={() => this.updateIdea(i)} />
 
-      <input type='text' name='description' value={idea.description} 
-        onChange={(event) => this.handleChange(event, i)} 
-        onBlur={() => this.updateIdea(i)}/>
+        <StyledDescription type='text' name='description' value={idea.description}
+          onChange={(event) => this.handleChange(event, i)}
+          onBlur={() => this.updateIdea(i)} />
       </StyledIdea>
       )
     })
 
     return (
       <div>
-      <StyledHeader>
-        <h1>{this.state.user.userName}'s Idea Board</h1>
-      </StyledHeader>
-      <StyledNav>
-        <StyledButton onClick={this.handleNew}>New Idea</StyledButton>
-        <StyledSort>Sort Ideas By:</StyledSort>
-      </StyledNav>
+        <StyledHeader>
+          <h1>{this.state.user.userName}'s Idea Board</h1>
+        </StyledHeader>
+        <StyledNav>
+          <StyledButton onClick={this.handleNew}>New Idea</StyledButton>
+          <StyledSort>Sort Ideas By:</StyledSort>
+        </StyledNav>
         <StyledIdeaList>{ideasList}</StyledIdeaList>
       </div>
     )
