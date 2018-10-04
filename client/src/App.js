b09609a4acdf9144fa978a13c17d73a1b9612f36
miknhoj <kimjohan86@gmail.com>
2018-10-04 11:28:19 -0400

    added routes on React app.js and added Home.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import IdeaBoard from './components/IdeaBoard';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <IdeaBoard />
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={IdeaBoard}></Route>
+        </Switch>
+      </Router>
     );
   }
 }
